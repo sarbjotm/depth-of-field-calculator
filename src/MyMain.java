83464652955060aaa706ee4028e6617522a92e95
Sarbjot <sarbjotmann98@gmail.com>
2020-05-26 15:27:37 -0700

    Completed show function

--- src/MyMain.java
+++ src/MyMain.java
@@ -1,5 +1,6 @@
 import Model.Lens;
 import Model.lensManager;
+import TextUi.textUi;
 
 import java.util.ArrayList;
 
@@ -7,15 +8,13 @@ public class MyMain {
     public static void main(String args[]) {
 
         lensManager manager = new lensManager();
+        textUi ui = new textUi(manager);
         manager.add(new Lens("Canon", 50, 1.8));
         manager.add(new Lens("Tamron", 90, 2.8));
         manager.add(new Lens("Sigma", 200, 2.8));
         manager.add(new Lens("Nikon", 200, 4.0));
 
-        for (Lens l : manager){
-            System.out.println("Lens is: " + l.getMake());
-        }
-
+        ui.show();
     }
 }
 
--- src/TextUi/textUi.java
+++ src/TextUi/textUi.java
@@ -1,9 +1,11 @@
 package TextUi;
 
+import Model.Lens;
 import Model.lensManager;
 
 public class textUi {
     private lensManager manager;
+    private int i = 0;
 
     public textUi(lensManager manager) {
         this.manager = manager;
@@ -12,10 +14,14 @@ public class textUi {
     public void show(){
         boolean isDone = false;
         while (!isDone){
-
+            for (Lens l : manager){
+                System.out.println( i + ". " + l.getMake() + " " + l.getMaximumAperture() +"mm F" + l.getFocalLength());
+                i = i + 1;
+            }
+            isDone = true;
         }
+
     }
-
-
-
 }
+
+
