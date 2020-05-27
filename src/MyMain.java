91132b3c4d381b53fb6e3a0850491d063e064aed
Sarbjot <sarbjotmann98@gmail.com>
2020-05-26 22:49:25 -0700

    Removed the creation of the cameras in main, instead they are created in the UI

--- src/MyMain.java
+++ src/MyMain.java
@@ -1,19 +1,11 @@
-import Model.Lens;
 import Model.lensManager;
 import TextUi.textUi;
-
-import java.util.ArrayList;
 
 public class MyMain {
     public static void main(String args[]) {
 
         lensManager manager = new lensManager();
         textUi ui = new textUi(manager);
-        manager.add(new Lens("Canon", 50, 1.8));
-        manager.add(new Lens("Tamron", 90, 2.8));
-        manager.add(new Lens("Sigma", 200, 2.8));
-        manager.add(new Lens("Nikon", 200, 4.0));
-
         ui.show();
     }
 }
